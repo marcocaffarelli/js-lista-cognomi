9ca578b8438a69001e39091e3bc4d832791645c9
marcocaffarelli <caffarelli-m94@hotmail.it>
2020-11-18 18:26:19 +0100

    Creata una lista delle variabili
    Creata una lista delle variabili

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,5 +1,7 @@
+// Lista variabili
+var cognomeUtente, cognomi, i, cognomeSingolo, cognomiStampati, posizione;
 // Cognome utente
-var cognomeUtente = prompt("Inserisci il tuo Cognome");
+cognomeUtente = prompt("Inserisci il tuo Cognome");
 // console.log("cognomeUtente");
 
 // Trasformare in maiuscola la prima lettera del cognomeUtente
@@ -7,33 +9,33 @@ cognomeUtente = cognomeUtente.charAt(0).toUpperCase() + cognomeUtente.slice(1);
 // console.log(cognomeUtente);
 
 // array cognomi
-var cognomi = [
+cognomi = [
   "Bianchi",
   "Rossi",
   "Duzioni",
   "Verdi",
   "Balsano"
-]
+];
 
 // aggiungere cognome all'array
-cognomi.push(cognomeUtente)
+cognomi.push(cognomeUtente);
 // console.log(cognomi);
 
 // ordinare alfabeticamente l'array
-cognomi.sort()
+cognomi.sort();
 // console.log(cognomi);
 
 // posizione del cognome utente nella lista
 // console.log(posizione);
 
 // stampa dei cognomi
-for (var i = 0; i < cognomi.length; i++) {
-  var cognomeSingolo = cognomi[i]
-  var cognomiStampati = document.getElementById("lista_cognomi").innerHTML;
+for (i = 0; i < cognomi.length; i++) {
+  cognomeSingolo = cognomi[i];
+  cognomiStampati = document.getElementById("lista_cognomi").innerHTML;
   document.getElementById("lista_cognomi").innerHTML = cognomiStampati + "<li>" + cognomeSingolo + "</li>";
 }
 
 // posizione cognome utente all'interno della lista
-var posizione = cognomi.indexOf(cognomeUtente) + 1;
+posizione = cognomi.indexOf(cognomeUtente) + 1;
 // stampa numero posizione cognome utente
-document.getElementById('posizione_cognome_utente').innerHTML = posizione
+document.getElementById('posizione_cognome_utente').innerHTML = "Il tuo cognome è  il numero " + posizione + " della lista";
