1d4671a4f776417870824397a9db80c7bb1ee766
marcocaffarelli <caffarelli-m94@hotmail.it>
2020-11-18 14:01:02 +0100

    Aggiunto cognome utente alla lista
    Aggiunto cognome utente alla lista

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,2 +1,14 @@
 var cognomeUtente = prompt("Inserisci il tuo Cognome");
-// console.log("cognomeUtente");
+//console.log("cognomeUtente");
+// array cognomi
+var cognomi = [
+  "Bianchi",
+  "Rossi",
+  "Duzioni",
+  "Verdi",
+  "Balsano"
+]
+
+// aggiungere cognome all array
+cognomi.push(cognomeUtente)
+// console.log(cognomi);
