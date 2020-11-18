da1131890926629c5fa8b527771563cba3ed45d2
marcocaffarelli <caffarelli-m94@hotmail.it>
2020-11-18 18:12:52 +0100

    Aggiunto il ciclo for per stampare la lista nell' html
    Aggiunto il ciclo for per stampare la lista nell' html

--- assets/js/main.js
+++ assets/js/main.js
@@ -24,5 +24,16 @@ cognomi.sort()
 // console.log(cognomi);
 
 // posizione del cognome utente nella lista
-var posizione = cognomi.indexOf(cognomeUtente) + 1;
 // console.log(posizione);
+
+// stampa dei cognomi
+for (var i = 0; i < cognomi.length; i++) {
+  var cognomeSingolo = cognomi[i]
+  var cognomiStampati = document.getElementById("lista_cognomi").innerHTML;
+  document.getElementById("lista_cognomi").innerHTML = cognomiStampati + "<li>" + cognomeSingolo + "</li>";
+}
+
+// posizione cognome utente all'interno della lista
+var posizione = cognomi.indexOf(cognomeUtente) + 1;
+// stampa numero posizione cognome utente
+document.getElementById('posizione_cognome_utente').innerHTML = posizione
